00b3911b58fdef799bc6e09da82624242cd94e8a
Muhammed Kılıç <3473393+kiliczsh@users.noreply.github.com>
2020-12-26 18:37:18 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,57 @@
+var eksisozluk = "https://eksisozluk.com";
+function getFaverList(){
+    let favsDoc = document.getElementsByClassName('favorite-list-popup toggles-menu')[0].getElementsByTagName('div')[0];
+    let faversList = favsDoc.getElementsByTagName('ul')[0].children;
+    let result = []
+    for(let i=0;i<faversList.length;i++){
+        let link = faversList[i].children[0].getAttribute('href');
+        if(link.startsWith('/biri/')){
+            result.push(link)
+        }
+    }
+    return result;
+}
+
+function getProfile(profile){
+    var requestOptions = { method: 'GET', redirect: 'follow' };
+    var data2 = fetch(profileUrl, requestOptions)
+        .then(response => response.text())
+        .then(result => {dataa = result; data2 = result;})
+        .catch(error => console.log('error', error));
+
+    return data2;
+}
+async function sendRequests(actionUrl){
+    var action = eksisozluk + actionUrl;
+    console.log(action);
+    await fetch(action, {
+        "credentials": "include",
+        "headers": {
+            "Accept": "*/*",
+            "Accept-Language": "en-US,en;q=0.5",
+            "X-Requested-With": "XMLHttpRequest"
+        },
+        "method": "POST",
+        "mode": "cors"
+    }).then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+}
+
+
+var faversList = getFaverList();
+var profileUrl = eksisozluk + faversList[0];
+console.log(profileUrl);
+var requestOptions = { method: 'GET', redirect: 'follow' };
+await fetch(profileUrl, requestOptions)
+    .then(response => response.text())
+    .then(result => {userData = result;})
+    .catch(error => console.log('error', error));
+var userDoc = new DOMParser().parseFromString(userData, "text/html");
+var blockedUser = userDoc.getElementById('blocked-link');
+var blockedEntries = userDoc.getElementById('blocked-index-title-link');
+var banUserUrl = blockedUser.getAttribute('data-remove-url');
+var banEntriesUrl = blockedEntries.getAttribute('data-remove-url');
+sendRequests(banUserUrl);
+sendRequests(banUserEntriesUrl);
+
